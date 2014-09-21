f338a76153782e536ba880f02a8c61d9c5e62355
martinorocco@gmail.com <martinorocco@gmail.com@0a0c8477-975a-c2c4-2d6d-125e3245055d>
2014-09-21 21:05:14 +0000

    fixes for vc++ build (link to osgUtil)

--- trunk/src/osgWrappers/serializers/pSound/Buffer_serializer.cpp
+++ trunk/src/osgWrappers/serializers/pSound/Buffer_serializer.cpp
@@ -27,6 +27,17 @@
 #include <pSound/Buffer>
 
 #include <osgDB/Registry>
+#include <osgDB/FileUtils>
+#include <osgDB/FileNameUtils>
+/* ....................................................................... */
+/* ======================================================================= */
+
+
+
+
+/* ======================================================================= */
+/* ....................................................................... */
+#define USE_INLINE_FILE 0
 /* ....................................................................... */
 /* ======================================================================= */
 
@@ -44,26 +55,127 @@ static bool checkData(const pSound::Buffer& buffer)
 static bool readData(osgDB::InputStream& is, pSound::Buffer& buffer)
 {
     if( is.isBinary() ) {
+#if ! USE_INLINE_FILE
         unsigned int    size = is.readSize() ;
         char*   data = (char*)malloc(size) ;
         is.readCharArray(data, size) ;
         buffer.setData(data) ;
+#else
+
+        unsigned int size = is.readSize();
+        if ( size>0 )
+        {
+            char* data = (char*) malloc( size ) ;
+            if ( !data )
+            {
+                is.throwException( "Buffer serializer: readData(): Out of memory." );
+                if ( is.getException() ) return false;
+            }
+            is.readCharArray( data, size );
+
+            std::string ext = osgDB::getFileExtension( buffer.getSoundFileName() );
+            osgDB::ReaderWriter* reader =
+                osgDB::Registry::instance()->getReaderWriterForExtension( ext );
+            if ( reader )
+            {
+                std::stringstream inputStream;
+                inputStream.write( data, size );
+
+                osgDB::ReaderWriter::ReadResult rr = reader->readObject( inputStream );
+                if ( rr.validObject() ) {
+                    osg::ref_ptr<osg::Object>       o = rr.takeObject();
+                    osg::ref_ptr<pSound::Buffer>    tmp = dynamic_cast<pSound::Buffer*>(o.get());
+
+                    if( tmp.valid() ) {
+
+                        buffer.setBitsPerSample     (   tmp->getBitsPerSample() ) ;
+                        buffer.setFrequency         (   tmp->getFrequency()     ) ;
+                        buffer.setNumChannels       (   tmp->getNumChannels()   ) ;
+                        buffer.setDataSize          (   tmp->getDataSize()      ) ;
+                        buffer.setData              (   tmp->releaseData()      ) ;
+                    }
+
+                }
+                else
+                {
+                    OSG_WARN << "Buffer serializer: readData(): "
+                                            << rr.message() << std::endl;
+                }
+            }
+            else
+            {
+                OSG_WARN << "Buffer serializer: readData(): Unable to find a plugin for "
+                                        << ext << std::endl;
+            }
+            free( data ) ;
+        }
+
+#endif
+
     }
+
     return true ;
 }
 
 static bool writeData(osgDB::OutputStream& os, const pSound::Buffer& buffer)
 {
     if( os.isBinary() ) {
+
+#if ! USE_INLINE_FILE
         unsigned int    size = buffer.getDataSize() ;
         os.writeSize(size) ;
         const char*     data = (const char*)buffer.getData() ;
         if( data && size ) {
             os.writeCharArray(data, size) ;
         }
+#else
+
+
+        std::string fullPath = osgDB::findDataFile( buffer.getSoundFileName() );
+        std::ifstream infile( fullPath.c_str(), std::ios::in|std::ios::binary );
+        if ( infile )
+        {
+            infile.seekg( 0, std::ios::end );
+            unsigned int size = infile.tellg();
+            os.writeSize(size);
+
+            if ( size>0 )
+            {
+                char* data = (char*) malloc( size ) ;
+                if ( !data )
+                {
+                    os.throwException( "Buffer serializer: writeData(): Out of memory." );
+                    if ( os.getException() ) return false;
+                }
+
+                infile.seekg( 0, std::ios::beg );
+                infile.read( data, size );
+                os.writeCharArray( data, size );
+                free( data ) ;
+            }
+            infile.close();
+        }
+        else
+        {
+            OSG_WARN << "Buffer serializer: writeData(): Failed to open sound file "
+                                << buffer.getSoundFileName() << std::endl;
+            os << (unsigned int)0;
+        }
+
+#endif
+
+
     }
+
+    else
+    {
+        os << std::endl ;
+    }
+
     return true ;
 }
+
+
 } // anon namespace
 /* ....................................................................... */
 /* ======================================================================= */
