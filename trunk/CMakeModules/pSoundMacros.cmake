766698ecaa1b6984adbc9615c18eff698af957ba
martinorocco@gmail.com <martinorocco@gmail.com@0a0c8477-975a-c2c4-2d6d-125e3245055d>
2014-11-20 22:42:19 +0000

    added PSOUND_ prefix to custom CMake vars

--- trunk/CMakeModules/pSoundMacros.cmake
+++ trunk/CMakeModules/pSoundMacros.cmake
@@ -188,7 +188,7 @@ MACRO(PSOUND_SETUP_PLUGIN)
 
         IF( WIN32 )
 
-            EXECUTE_PROCESS(COMMAND ${OSG_BINARIES}\\osgversion.exe --version-number OUTPUT_VARIABLE OSG_VERSION)
+            EXECUTE_PROCESS(COMMAND ${PSOUND_OSG_BINARIES}\\osgversion.exe --version-number OUTPUT_VARIABLE OSG_VERSION)
             STRING(REGEX REPLACE "\n" "" OSG_VERSION "${OSG_VERSION}")
 
         ELSE( WIN32 )
